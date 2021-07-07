116db8794ded7d8f90173e256ad09b5072346ed2
0xEquinox <69062137+0xEquinox@users.noreply.github.com>
2021-07-07 16:59:18 -0700

    Added the C++ version

---
+++ C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+ int num = 100;
+
+ for (int i = 0; i < num; i++)
+    {
+        if (i%15 == 0)       
+            printf ("FizzBuzz\n");   
+      
+        else if ((i%3) == 0)   
+            printf("Fizz\n");                
+    
+        else if ((i%5) == 0)                      
+            printf("Buzz\n");                
+     
+        else        
+            printf("%d\n", i);                
+    }
+}
