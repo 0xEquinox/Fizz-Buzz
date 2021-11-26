5a8030c904040dc8cd811cabccc656957829cb1a
0xEquinox <69062137+0xEquinox@users.noreply.github.com>
2021-11-25 20:18:38 -0800

    Added Nim version

---
+++ Nim.nim
@@ -0,0 +1,14 @@
+proc fizz_buzz(n: int) =
+
+  for i in 1..n:
+    
+    if i mod 15 == 0:
+      echo "FizzBuzz"
+    elif i mod 5 == 0:
+      echo "Buzz"
+    elif i mod 3 == 0:
+      echo "Fizz"
+    else:
+      echo i
+
+fizz_buzz(100)
