ee206fcc4357605f19133faf475f1c861d71ee89
0xEquinox <69062137+0xEquinox@users.noreply.github.com>
2021-12-12 17:12:56 -0800

    Fixed Ruby

--- ruby.rb
+++ ruby.rb
@@ -1,13 +1,16 @@
-num = 100
 
-for i in 0..num
-  if i % 5 == 0 and i % 3 == 0
-    puts "FizzBuzz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  elsif i % 3 == 0
-    puts "Fizz"
-  else
-    puts "#{i}"
-  end
+def fizz_buzz(n)
+    for i in 0..n
+      if i % 5 == 0 and i % 3 == 0
+        puts "FizzBuzz"
+      elsif i % 5 == 0
+        puts "Buzz"
+      elsif i % 3 == 0
+        puts "Fizz"
+      else
+        puts "#{i}"
+      end
+    end
 end
+
+fizz_buzz(100)
